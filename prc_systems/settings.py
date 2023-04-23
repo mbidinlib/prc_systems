0627fe40a4a8a640b0eeac67bfb0230e14d49bed
Mathew Bidinlib <61548432+mbidinlib@users.noreply.github.com>
2023-04-23 03:20:56 -0500

    updated

--- prc_systems/settings.py
+++ prc_systems/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': 'prc_systems',
         'USER': 'postgres',
         'PASSWORD': 'GloryToJesus@1',
-        'HOST': 'https://33l8e09lql.execute-api.us-west-2.amazonaws.com/',
+        'HOST': '33l8e09lql.execute-api.us-west-2.amazonaws.com/',
         'PORT': '5432',
     }
 }
