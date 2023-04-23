05365f5ed3262542b46a42546e04d7ba9dd536f3
Mathew Bidinlib <61548432+mbidinlib@users.noreply.github.com>
2023-04-22 20:44:16 -0500

    removed vercel file

--- prc_systems/settings.py
+++ prc_systems/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-9pm!m=yg105f@crp0o!7&i%m%q-(z(kh1udn05^5e(t#4715e3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- prc_systems/wsgi.py
+++ prc_systems/wsgi.py
@@ -14,6 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prc_systems.settings')
 
 application = get_wsgi_application()
-
-# for vercel
-app = application
